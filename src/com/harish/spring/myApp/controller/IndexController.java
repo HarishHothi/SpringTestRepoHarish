4e73484cf47fec2a66423365889ff11dd75f2d4a
HarishHothi <hothi.harish@gmail.com>
2015-06-12 19:24:12 +0530

    finally hello world working

---
+++ src/com/harish/spring/myApp/controller/IndexController.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.harish.spring.myApp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Harish.A
+ * controller must be annoted with @Controller annotation
+ * it must have one method with annotation @RequestMapping 
+ */
+
+@Controller
+public class IndexController {
+
+	@RequestMapping("/index")
+	public String getIndexPage(){
+		System.out.println("Here comes request");
+		return "/WEB-INF/index1.jsp";
+	}
+}
