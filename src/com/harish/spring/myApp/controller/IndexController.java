5b1f66a24ac169e04c75880c540766cc941a326a
HarishHothi <hothi.harish@gmail.com>
2015-06-15 17:05:10 +0530

    Hibernate setup,UI complete

--- src/com/harish/spring/myApp/controller/IndexController.java
+++ src/com/harish/spring/myApp/controller/IndexController.java
@@ -18,6 +18,6 @@ public class IndexController {
 	@RequestMapping("/index")
 	public String getIndexPage(){
 		System.out.println("Here comes request");
-		return "/WEB-INF/index1.jsp";
+		return "index";
 	}
 }
