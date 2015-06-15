f45bae62b144f3cff263b57d51697aa756d56fdf
HarishHothi <hothi.harish@gmail.com>
2015-06-15 19:21:19 +0530

    Blog database Ready

---
+++ src/com/harish/spring/myApp/entity/Role.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package com.harish.spring.myApp.entity;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+/**
+ * @author Harish.A
+ *
+ */
+@Entity
+public class Role {
+
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	private String name;
+	
+	@ManyToMany
+	@JoinTable
+	private List<User> users;
+
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
